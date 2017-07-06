7d5e202369781c1f6574d888a83b7bfac680197e
Zohar Galor <zgalor@redhat.com>
2017-07-06 16:00:59 +0300

    Add AlertManager pod to prometheus depolyment
    Prometheus Pod: prometheus behind oauth-proxy
    Alerts Pod: alertmanager behind message-buffer behind oauth-proxy

--- openshift/create_prometheus.sh
+++ openshift/create_prometheus.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # run from master to create prometheus
-oc process -f prometheus_template.yaml -p SERVICE_SIGNER_CERT="$(cat /etc/origin/master/service-signer.crt)" -p PROMETHEUS_HOSTNAME="prometheus.${HOSTNAME}" | oc create -f -
+oc create namespace prometheus
+oc project prometheus
+oc process -f prometheus_template.yaml -p NAMESPACE="prometheus" -p SERVICE_SIGNER_CERT="$(cat /etc/origin/master/service-signer.crt)" | oc create -f -
 
